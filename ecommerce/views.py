66cb36f1e01ec7ced519e80bc7b75974eb6e64d2
Mayinis <hachhe.dev@gmail.com>
2022-05-11 22:59:39 -0400

    products module

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,11 @@
 from re import template
 from django.shortcuts import render
+from store.models import Product
+
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all().filter(is_available=True)
+    diccionary = {
+        'products' : products
+    }
+    return render(request, 'home.html', diccionary)
