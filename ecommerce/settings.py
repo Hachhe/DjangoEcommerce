6851b886f94ea7046e55020dfc10e48c74fd5f4e
Mayinis <hachhe.dev@gmail.com>
2022-05-30 11:37:43 -0400

    context preprocessor category

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from unicodedata import category
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_preprocessor.menu_list',
             ],
         },
     },
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,7 +2,6 @@ from re import template
 from django.shortcuts import render
 from store.models import Product
 
-
 def home(request):
     products = Product.objects.all().filter(is_available=True)
     diccionary = {
